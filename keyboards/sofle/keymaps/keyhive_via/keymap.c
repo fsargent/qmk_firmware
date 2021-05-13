e4b2d30b35490667fa849fbf676be3b4665bbdd6
Felix Sargent <felix.sargent@gmail.com>
2021-05-13 08:07:03 -0700

    Stable.

--- keyboards/sofle/keymaps/keyhive_via/keymap.c
+++ keyboards/sofle/keymaps/keyhive_via/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_PLUS,
   KC_NO,  LGUI_T(KC_A),LALT_T(KC_S),LCTL_T(KC_D),LSFT_T(KC_F),KC_G,       KC_H,LSFT_T(KC_J),LCTL_T(KC_K),LALT_T(KC_L),LGUI_T(KC_SCLN),  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, S(A(KC_A)),               KC_MEDIA_PLAY_PAUSE,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_SFTENT,
-  KC_LCTRL,KC_LALT, LT(MEDR, KC_ESC),  LT(NAVR, KC_SPC),  LT(MOUR, KC_TAB),  LT(NSSL, KC_ENT),  LT(NSL, KC_BSPC),  LT(FUNL, KC_DEL),  KC_RALT, TG(1)
+  KC_LCTRL,KC_LALT, LT(MEDR, KC_ESC),  LT(NAVR,KC_BSPC ),  LT(MOUR, KC_TAB),  LT(NSSL, KC_ENT),  LT(NSL, KC_SPC),  LT(FUNL, KC_DEL),  KC_RALT, TG(1)
 ),
 /*
  * GAMING
