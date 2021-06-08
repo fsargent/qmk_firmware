60700c03bb4ad9b6644226e98831c9160761040e
Felix Sargent <felix.sargent@gmail.com>
2021-06-07 18:52:53 -0700

    Added braces to right hand

--- keyboards/sofle/keymaps/fsargent/keymap.c
+++ keyboards/sofle/keymaps/fsargent/keymap.c
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,             KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,
     MT(MOD_MEH,KC_ESC), KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                                       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT,            KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, S(G(KC_A)),                  KC_MPLY, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT,
-                        KC_LALT, KC_LCTL, MO(SYM), CMD_T(KC_BSPC), LT(NAV, KC_TAB),   LT(SYM, KC_ENT),  KC_SPC,  LT(NAV,KC_ESC),  C(G(KC_SPC)), TD(LAYR)
+                        KC_LALT, KC_LCTL, MO(SYM), CMD_T(KC_BSPC), LT(NAV, KC_TAB),   LT(SYM, KC_ENT),  KC_SPC,  LT(NAV,KC_ESC), KC_LBRC, KC_RBRC
   ),
   [WIN] = LAYOUT(
     _______,KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     _______,
@@ -88,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,    LGUI(KC_LEFT), LGUI(KC_DOWN),   LGUI(KC_UP),    LGUI(KC_RGHT),  KC_ENT,                            KC_VOLD,    KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_SCROLLLOCK,
     KC_LALT,    KC_LEFT,       KC_DOWN,         KC_UP,          KC_RGHT,        KC_SPC,                            KC_MS_BTN4, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_PAUSE,
     KC_LSFT,    S(A(KC_LEFT)), LALT(KC_LEFT),   LALT(KC_RGHT),  S(A(KC_RGHT)), LALT(KC_BSPC), MEH(KC_G),      TO(3),  KC_MS_BTN5, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_INSERT,
-                               KC_RST,          _______,        _______,        _______,      _______,     KC_MS_BTN1,KC_MS_BTN2,KC_MS_BTN3, _______, _______
+                               KC_RST,          _______,        _______,        _______,      _______,     KC_MS_BTN1,KC_MS_BTN2,KC_MS_BTN3, _______, TD(LAYR)
   ),
   [NUM] = LAYOUT(
     KC_ESC,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,                     KC_F6,    KC_F7,    KC_F8,      KC_F9,      KC_F10,     KC_F11,
@@ -101,8 +101,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,     KC_F1,    KC_F2,      KC_F3,  KC_F4,  KC_F5,                    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_F11,
     _______,    KC_SLSH,  KC_GRV,     KC_MINS,KC_LCBR,KC_RCBR,                  KC_PSLS,  KC_P7,    KC_P8,    KC_P9,    KC_PPLS,   KC_F12,
     _______,    KC_PIPE,  KC_MINS,    KC_EQL, KC_LPRN,KC_RPRN,                  KC_PAST,  KC_P4,    KC_P5,    KC_P6,    KC_PEQL,   KC_PSCR,
-    _______,    KC_BSLS,  KC_UNDS,    KC_PLUS,KC_LBRC,KC_RBRC,_______,  TO(0),  KC_PMNS,  KC_P1,    KC_P2,    KC_P3,    KC_PENT,   KC_NLCK,
-                          KC_RST,     _______,_______,_______,_______,  _______,_______,  _______,  _______,  _______
+    _______,    KC_BSLS,  KC_UNDS,    KC_PLUS,KC_LBRC,KC_RBRC, C(G(KC_SPC)),  TO(0),  KC_PMNS,  KC_P1,    KC_P2,    KC_P3,    KC_PENT,   KC_NLCK,
+                          KC_RST,     _______,_______,_______,_______,  _______,_______,  _______,  KC_LPRN,  KC_RPRN
   ),
    [GAME] = LAYOUT(
     KC_ESC,    KC_ESC,     KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_BSPC,
